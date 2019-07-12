575ad083bc3f1f24c73b59be0256f4cee73e268d
LordLazarus <46389911+LordLazarus@users.noreply.github.com>
2019-07-12 16:05:25 -0500

    fixed linting errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,30 @@
-const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-const watch = require('gulp-watch');
-const lec = require('gulp-line-ending-corrector');
+const gulp = require("gulp");
+const eslint = require("gulp-eslint");
+const _watch = require("gulp-watch");
+const lec = require("gulp-line-ending-corrector");
 //const uglify = require('gulp-uglify-es').default;
 
-gulp.task('lint', () => {
-    return gulp.src(['src/**/*.js'])
-        .pipe(lec())
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+gulp.task("lint", () =>
+{
+   return gulp.src(["src/**/*.js"])
+      .pipe(lec())
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
 });
 
-gulp.task("compress", function () {
-    return gulp.src("src/**/*.js")
-        //.pipe(uglify())
-        .pipe(gulp.dest("build/"));
+gulp.task("compress", function ()
+{
+   return gulp.src("src/**/*.js")
+   //.pipe(uglify())
+      .pipe(gulp.dest("build/"));
 });
 
-gulp.task('default', ['lint', 'compress']);
+gulp.task("default", ["lint", "compress"]);
 
-gulp.task('build', ['lint', 'compress']);
+gulp.task("build", ["lint", "compress"]);
 
-gulp.task('watch', function() {
-  gulp.watch('src/**/*.js', ['default']);
+gulp.task("watch", function()
+{
+   gulp.watch("src/**/*.js", ["default"]);
 });
