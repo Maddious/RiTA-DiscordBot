073e35dfb142d39211f454a213303a42c80468db
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2019-07-13 21:24:22 +0100

    1.1.1

--- src/core/status.js
+++ src/core/status.js
@@ -11,7 +11,7 @@ module.exports = function(bot, status, channel, writable = true)
          bot.setPresence({
             status: "online",
             game: {
-               name: "!t help / !translate help V1.1.0"
+               name: "!t help / !translate help V1.1.1"
             }
          });
       },
