30538232ded9c1bb2f9eb9a8223d5dbe8614e62a
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2019-09-12 04:07:15 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -24,24 +24,29 @@ function compress()
    //.pipe(uglify())
       .pipe(gulp.dest("build"));
 }
-gulp.task("compress", compress);
-gulp.task("lint", lint);
-gulp.task("watch", GulpWatch);
-gulp.task("default", gulp.parallel(lint, compress));
-gulp.task("build", gulp.parallel(lint, compress));
+
 function GulpWatch()
 {
    gulp.watch("src/**/*.js", gulp.series("default"));
 }
 
-gulp.task("coveralls", function (done) {
-  if (!process.env.CI) {
-    done();
-  } else {
-    return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-      .pipe(coveralls());
-  }
-});
+//function coverage()
+//{
+//gulp.src('coverage/lcov.info')
+//  .pipe(coveralls());
+//}
 
-gulp.src('test/coverage/**/lcov.info')
-  .pipe(coveralls());
+gulp.task("compress", compress);
+gulp.task("lint", lint);
+gulp.task("watch", GulpWatch);
+gulp.task("default", gulp.parallel(lint, compress));
+gulp.task("build", gulp.parallel(lint, compress));
+
+//gulp.task("coveralls", function (done) {
+//  if (!process.env.CI) {
+//    done();
+//  } else {
+//    return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+//      .pipe(coveralls());
+//  }
+//});
