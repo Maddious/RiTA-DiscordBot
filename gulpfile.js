33c5ede563e556eb7b452e6884d854cd7f6fe267
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2019-09-12 03:00:28 +0100

    Coveralls V3

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp");
 const eslint = require("gulp-eslint");
 const watch = require("gulp-watch");
 const lec = require("gulp-line-ending-corrector");
+var coveralls = require('@kollavarsham/gulp-coveralls');
 //const uglify = require('gulp-uglify-es').default;
 
 //tasks
@@ -32,3 +33,6 @@ function GulpWatch()
 {
    gulp.watch("src/**/*.js", gulp.series("default"));
 }
+
+gulp.src('test/coverage/**/lcov.info')
+  .pipe(coveralls());
