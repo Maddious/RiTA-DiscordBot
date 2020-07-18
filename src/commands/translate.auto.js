4d7e9be7e9cb26bc2bd7893378d1170f4571f612
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2020-07-18 17:07:58 +0100

    Update translate.auto.js

--- src/commands/translate.auto.js
+++ src/commands/translate.auto.js
@@ -28,9 +28,26 @@ module.exports = function(data)
 
    if (data.cmd.from === "auto" || data.cmd.from.valid.length !== 1)
    {
-      data.color = "error";
+      //
+      // Add task to database
+      //
+
+      db.addTask(data.task);
+
+      //
+      // Send out success message
+      //
+
+      const langTo = data.cmd.to.valid[0].name;
+      const forNames = data.cmd.for.join(",  ").replace(
+         "me", `<@${data.message.author.id}>`
+      );
+
+      data.color = "ok";
       data.text =
-         ":warning:  Please specify a valid language to translate from.";
+         ":white_check_mark:  Automatically translating messages " +
+         `from Automatic Detection to **\`${langTo}\`** ` +
+         `for ${forNames}.`;
 
       return botSend(data);
    }
@@ -256,22 +273,13 @@ module.exports = function(data)
       const forNames = data.cmd.for.join(",  ").replace(
          "me", `<@${data.message.author.id}>`
       );
-      const langExceptions =
-      {
-         "zh-CN": "chinese simplified",
-         "zh-TW": "chinese traditional",
-         "fr": "French"
-      };
 
       data.color = "ok";
-      langData.text = ":white_check_mark: Language Test " + `${langTo}`;
       data.text =
          ":white_check_mark:  Automatically translating messages " +
-         `from **\`${langFrom}\`** to **\`${langExceptions, langTo}\`** ` +
+         `from **\`${langFrom}\`** to **\`${langTo}\`** ` +
          `for ${forNames}.`;
 
-
       return botSend(data);
-      return botSend(langData);
    };
 };
