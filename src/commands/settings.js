d47210ecd9ff1858b21164cf17a7c9937425d940
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2020-09-13 17:55:01 +0100

    Update settings.js

--- src/commands/settings.js
+++ src/commands/settings.js
@@ -206,10 +206,10 @@ const getSettings = function(data)
 
    const b2b = function(data)
    {
-      const commandVariable0 = data.cmd.params.split(" ")[0].toLowerCase();
-      const commandVariable1 = data.cmd.params.split(" ")[1].toLowerCase();
+      var commandVariable0 = data.cmd.params.split(" ")[0].toLowerCase();
+      var commandVariable1 = data.cmd.params.split(" ")[1].toLowerCase();
 
-      const validState = 
+      const validState =
       {
          "on": true,
          "off": false
@@ -217,18 +217,18 @@ const getSettings = function(data)
       
       if (commandVariable1 = validState)
       {
-         b2bvar = commandVariable1
-         var output = 
+         b2bvar = commandVariable1;
+         var output =
          "**```Bot 2 Bot Command Here```**\n" +
          `Embeded Messages : ${b2bvar}\n\n` +
          `Command Variable 0 : ${commandVariable0}\n` +
-         `Command Variable 1 : ${commandVariable1}\n` ;
-  
+         `Command Variable 1 : ${commandVariable1}\n`;
+
          data.color = "info";
          data.text = output;
          return botSend(data);
       }
-      else
+      
       data.color = "error";
       data.text =
          ":warning:  **`" + data.cmd.params +
