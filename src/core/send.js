f70b454e20f31e3a24ed934b1d2bc66009328509
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2020-09-14 00:19:59 +0100

    Update send.js

--- src/core/send.js
+++ src/core/send.js
@@ -13,211 +13,230 @@ const settings = require("../commands/settings");
 //
 // Send Data to Channel
 //
-if (settings.getEmbedVar() === "on" )
+module.exports = function(data)
 {
-   const sendBox = function(data)
+   if (settings.getEmbedVar() === "on" )
    {
-      if (data.author)
+      const sendBox = function(data)
       {
-         data.author = {
-            name: data.author.username,
-            //eslint-disable-next-line camelcase
-            icon_url: data.author.displayAvatarURL
-         };
-      }
-
-      if (data.text && data.text.length > 1)
-      {
-         data.channel.send({
-            embed: {
-               title: data.title,
-               fields: data.fields,
-               author: data.author,
-               color: colors.get(data.color),
-               description: data.text,
-               footer: data.footer
-            }
-         }).then(() =>
+         if (data.author)
          {
-            sendEmbeds(data);
-            sendAttachments(data);
-         }).catch(err =>
+            data.author = {
+               name: data.author.username,
+               //eslint-disable-next-line camelcase
+               icon_url: data.author.displayAvatarURL
+            };
+         }
+
+         if (data.text && data.text.length > 1)
          {
-            var errMsg = err;
-            logger("dev", err);
-
-            //
-            // Error for long messages
-            //
-
-            if (err.code && err.code === 50035)
+            data.channel.send({
+               embed: {
+                  title: data.title,
+                  fields: data.fields,
+                  author: data.author,
+                  color: colors.get(data.color),
+                  description: data.text,
+                  footer: data.footer
+               }
+            }).then(() =>
             {
-               data.channel.send(":warning:  Message is too long.");
-            }
-
-            //
-            // Handle error for users who cannot recieve private messages
-            //
-
-            if (err.code && err.code === 50007 && data.origin)
+               sendEmbeds(data);
+               sendAttachments(data);
+            }).catch(err =>
             {
-               const badUser = data.channel.recipient;
-               errMsg = `@${badUser.username}#${badUser.discriminator}\n` + err;
+               var errMsg = err;
+               logger("dev", err);
 
-               db.removeTask(data.origin.id, `@${badUser.id}`, function(er)
+               //
+               // Error for long messages
+               //
+
+               if (err.code && err.code === 50035)
                {
-                  if (er)
-                  {
-                     return logger("error", er);
-                  }
+                  data.channel.send(":warning:  Message is too long.");
+               }
 
-                  return data.origin.send(
-                     `:no_entry: User ${badUser} cannot recieve direct messages ` +
-                     `by bot because of **privacy settings**.\n\n__Auto ` +
-                     `translation has been stopped. To fix this:__\n` +
-                     "```prolog\nServer > Privacy Settings > " +
-                     "'Allow direct messages from server members'\n```"
-                  );
+               //
+               // Handle error for users who cannot recieve private messages
+               //
+
+               if (err.code && err.code === 50007 && data.origin)
+               {
+                  const badUser = data.channel.recipient;
+                  errMsg = `@${badUser.username}#${badUser.discriminator}\n` + err;
+
+                  db.removeTask(data.origin.id, `@${badUser.id}`, function(er)
+                  {
+                     if (er)
+                     {
+                        return logger("error", er);
+                     }
+
+                     return data.origin.send(
+                        `:no_entry: User ${badUser} cannot recieve direct messages ` +
+                        `by bot because of **privacy settings**.\n\n__Auto ` +
+                        `translation has been stopped. To fix this:__\n` +
+                        "```prolog\nServer > Privacy Settings > " +
+                        "'Allow direct messages from server members'\n```"
+                     );
+                  });
+               }
+
+               logger("error", errMsg);
+            });
+         }
+         else if (data.attachments.array().length > 0)
+         {
+            sendAttachments(data);
+         }
+      };
+
+
+      //
+      // Resend embeds from original message
+      // Only if content is forwared to another channel
+      //
+
+      const sendEmbeds = function(data)
+      {
+         if (data.forward && data.embeds && data.embeds.length > 0)
+         {
+            const maxEmbeds = data.config.maxEmbeds;
+
+            if (data.embeds.length > maxEmbeds)
+            {
+               sendBox({
+                  channel: data.channel,
+                  text: `:warning:  Cannot embed more than ${maxEmbeds} links.`,
+                  color: "warn"
                });
+
+               data.embeds = data.embeds.slice(0, maxEmbeds);
             }
 
-            logger("error", errMsg);
-         });
-      }
-      else if (data.attachments.array().length > 0)
-      {
-         sendAttachments(data);
-      }
-   };
-
-
-   //
-   // Resend embeds from original message
-   // Only if content is forwared to another channel
-   //
-
-   const sendEmbeds = function(data)
-   {
-      if (data.forward && data.embeds && data.embeds.length > 0)
-      {
-         const maxEmbeds = data.config.maxEmbeds;
-
-         if (data.embeds.length > maxEmbeds)
-         {
-            sendBox({
-               channel: data.channel,
-               text: `:warning:  Cannot embed more than ${maxEmbeds} links.`,
-               color: "warn"
-            });
-
-            data.embeds = data.embeds.slice(0, maxEmbeds);
+            for (let i = 0; i < data.embeds.length; i++)
+            {
+               data.channel.send(data.embeds[i].url);
+            }
          }
-
-         for (let i = 0; i < data.embeds.length; i++)
-         {
-            data.channel.send(data.embeds[i].url);
-         }
-      }
-   };
-
-   //
-   // Resend attachments
-   //
-
-   const sendAttachments = function(data)
-   {
-      var attachments = data.attachments.array();
-
-      if (data.forward && attachments && attachments.length > 0)
-      {
-         const maxAtt = data.config.maxEmbeds;
-
-         if (attachments.length > maxAtt)
-         {
-            sendBox({
-               channel: data.channel,
-               text: `:warning:  Cannot attach more than ${maxAtt} files.`,
-               color: "warn"
-            });
-            attachments = attachments.slice(0, maxAtt);
-         }
-
-         for (let i = 0; i < attachments.length; i++)
-         {
-            const attachmentObj = new discord.Attachment(
-               attachments[i].url,
-               attachments[i].filename
-            );
-            data.channel.send(attachmentObj);
-         }
-      }
-   };
-
-   //
-   // Analyze Data and determine sending style (system message or author box)
-   //
-
-   //eslint-disable-next-line complexity
-   module.exports = function(data)
-   {
-      var sendData = {
-         title: data.title,
-         fields: data.fields,
-         config: data.config,
-         channel: data.message.channel,
-         color: data.color,
-         text: data.text,
-         footer: data.footer,
-         embeds: data.message.embeds,
-         attachments: data.message.attachments,
-         forward: data.forward,
-         origin: null,
-         bot: data.bot
       };
 
       //
-      // Notify server owner if bot cannot write to channel
+      // Resend attachments
       //
 
-      if (!data.canWrite)
+      const sendAttachments = function(data)
       {
-         const writeErr =
-            ":no_entry:  **Translate bot** does not have permission to write at " +
-            `the **${sendData.channel.name}** channel on your server **` +
-            `${sendData.channel.guild.name}**. Please fix.`;
+         var attachments = data.attachments.array();
 
-         return sendData.channel.guild.owner.send(writeErr).catch(err =>
-            logger("error", err)
-         );
-      }
-
-      if (data.forward)
-      {
-         const forwardChannel = data.client.channels.get(data.forward);
-
-         if (forwardChannel)
+         if (data.forward && attachments && attachments.length > 0)
          {
-            //
-            // Check if bot can write to destination channel
-            //
+            const maxAtt = data.config.maxEmbeds;
 
-            var canWriteDest = true;
-
-            if (forwardChannel.type === "text")
+            if (attachments.length > maxAtt)
             {
-               canWriteDest = fn.checkPerm(
-                  forwardChannel.guild.me, forwardChannel, "SEND_MESSAGES"
+               sendBox({
+                  channel: data.channel,
+                  text: `:warning:  Cannot attach more than ${maxAtt} files.`,
+                  color: "warn"
+               });
+               attachments = attachments.slice(0, maxAtt);
+            }
+
+            for (let i = 0; i < attachments.length; i++)
+            {
+               const attachmentObj = new discord.Attachment(
+                  attachments[i].url,
+                  attachments[i].filename
                );
+               data.channel.send(attachmentObj);
             }
+         }
+      };
 
-            if (canWriteDest)
+      //
+      // Analyze Data and determine sending style (system message or author box)
+      //
+
+      //eslint-disable-next-line complexity
+      //module.exports = function(data)
+      {
+         var sendData = {
+            title: data.title,
+            fields: data.fields,
+            config: data.config,
+            channel: data.message.channel,
+            color: data.color,
+            text: data.text,
+            footer: data.footer,
+            embeds: data.message.embeds,
+            attachments: data.message.attachments,
+            forward: data.forward,
+            origin: null,
+            bot: data.bot
+         };
+
+         //
+         // Notify server owner if bot cannot write to channel
+         //
+
+         if (!data.canWrite)
+         {
+            const writeErr =
+               ":no_entry:  **Translate bot** does not have permission to write at " +
+               `the **${sendData.channel.name}** channel on your server **` +
+               `${sendData.channel.guild.name}**. Please fix.`;
+
+            return sendData.channel.guild.owner.send(writeErr).catch(err =>
+               logger("error", err)
+            );
+         }
+
+         if (data.forward)
+         {
+            const forwardChannel = data.client.channels.get(data.forward);
+
+            if (forwardChannel)
             {
-               sendData.origin = sendData.channel;
-               sendData.channel = forwardChannel;
+               //
+               // Check if bot can write to destination channel
+               //
+
+               var canWriteDest = true;
+
+               if (forwardChannel.type === "text")
+               {
+                  canWriteDest = fn.checkPerm(
+                     forwardChannel.guild.me, forwardChannel, "SEND_MESSAGES"
+                  );
+               }
+
+               if (canWriteDest)
+               {
+                  sendData.origin = sendData.channel;
+                  sendData.channel = forwardChannel;
+               }
+
+               //
+               // Error if bot cannot write to dest
+               //
+
+               else
+               {
+                  sendData.footer = null;
+                  sendData.embeds = null;
+                  sendData.color = "error";
+                  sendData.text =
+                     ":no_entry:  Bot does not have permission to write at the " +
+                     `<#${forwardChannel.id}> channel.`;
+
+                  return sendBox(sendData);
+               }
             }
 
             //
-            // Error if bot cannot write to dest
+            // Error on invalid forward channel
             //
 
             else
@@ -225,43 +244,27 @@ if (settings.getEmbedVar() === "on" )
                sendData.footer = null;
                sendData.embeds = null;
                sendData.color = "error";
-               sendData.text =
-                  ":no_entry:  Bot does not have permission to write at the " +
-                  `<#${forwardChannel.id}> channel.`;
-
+               sendData.text = ":warning:  Invalid channel.";
                return sendBox(sendData);
             }
          }
 
-         //
-         // Error on invalid forward channel
-         //
-
-         else
+         if (data.showAuthor)
          {
-            sendData.footer = null;
-            sendData.embeds = null;
-            sendData.color = "error";
-            sendData.text = ":warning:  Invalid channel.";
-            return sendBox(sendData);
+            sendData.author = data.message.author;
+
+            if (data.author)
+            {
+               sendData.author = data.author;
+            }
          }
-      }
 
-      if (data.showAuthor)
-      {
-         sendData.author = data.message.author;
+         return sendBox(sendData);
+      };
 
-         if (data.author)
-         {
-            sendData.author = data.author;
-         }
-      }
+   }
+   else
+   {
 
-      return sendBox(sendData);
-   };
-
-}
-else
-{
-
+   }
 }
