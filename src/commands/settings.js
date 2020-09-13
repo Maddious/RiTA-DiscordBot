8f8e69f7d1be6a98ece6b3c7e4d6a4d1f589f22e
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2020-09-13 20:17:25 +0100

    Update settings.js

--- src/commands/settings.js
+++ src/commands/settings.js
@@ -1,8 +1,8 @@
 const botSend = require("../core/send");
 const db = require("../core/db");
 const logger = require("../core/logger");
-var embedVar = "on";
-var b2bVar = "off";
+const embedVar = "on";
+const b2bVar = "off";
 // -------------------------
 // Proccess settings params
 // -------------------------
