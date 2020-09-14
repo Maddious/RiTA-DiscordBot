251eb5b90f4f32d6615ddf96efd659ae11ccdb36
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2020-09-14 02:13:44 +0100

    Update args.js

--- src/commands/args.js
+++ src/commands/args.js
@@ -221,7 +221,7 @@ module.exports = function(data)
          "info": cmdHelp,
          "list": cmdList,
          "stats": cmdStats,
-         "embed": cmdEmbed.value,
+         "embed": cmdEmbed,
          "version": cmdVersion,
          "invite": cmdMisc.invite,
          "shards": cmdMisc.shards,
