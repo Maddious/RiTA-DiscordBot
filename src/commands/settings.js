4f664d3f4effaa33b30da8f1cabd3a6f14032877
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2020-09-14 02:27:24 +0100

    Update settings.js

--- src/commands/settings.js
+++ src/commands/settings.js
@@ -211,33 +211,10 @@ const getSettings = function(data)
    };
 
    // --------------------------------------
-   // Embed Messages
+   // Bot to Bot Messages
    // --------------------------------------
 
-   const embed = function(data)
-   {
-      const commandVariable1 = data.cmd.params.split(" ")[1].toLowerCase();
-
-      if (commandVariable1 === "on" || commandVariable1 === "off")
-      {
-         embedVar = commandVariable1;
-         var output =
-         "**```Embedded Message Translation```**\n" +
-         `Embedded Message Translation is now turned : ${embedVar}\n\n`;
-
-         data.color = "info";
-         data.text = output;
-         return botSend(data);
-      }
-
-      data.color = "error";
-      data.text =
-         ":warning:  **`" + commandVariable1 +
-         "`** is not a valid embed option.";
-      return botSend(data);
-   };
-
-   const b2b = function(data)
+    const b2b = function(data)
    {
       const commandVariable1 = data.cmd.params.split(" ")[1].toLowerCase();
 
