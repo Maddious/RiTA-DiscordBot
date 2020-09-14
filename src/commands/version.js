1ae6f9a8e588b7e8ec401a6aa236d9fb8397da08
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2020-09-14 01:39:50 +0100

    Update version.js

--- src/commands/version.js
+++ src/commands/version.js
@@ -1,9 +1,5 @@
-const langCheck = require("../core/lang.check");
 const botSend = require("../core/send");
-const db = require("../core/db");
 const auth = require("../core/auth");
-const logger = require("../core/logger");
-const settings = require("./settings");
 
 module.exports = function(data)
 {
@@ -16,5 +12,5 @@ module.exports = function(data)
 
    data.color = "info";
    data.text = `:robot:  Current bot version is ${version}`;
-   return botSend(data);
+   botSend(data);
 };
