77bdd7d14f036140c6d0fb7ab3bdabba56a09860
Artanis_ <ledragio@gmail.com>
2020-10-07 07:50:16 -0400

    Update send.js

--- src/core/send.js
+++ src/core/send.js
@@ -23,6 +23,12 @@ module.exports = function(data)
    
    if (data.author)
    {
+if data.text.includes("<a"){
+        const regex = /<(a)([:+\s:\s*[a-z0-9ЁёА-я_\s]+:\s*)([0-9\s]+)>/gmi;
+const str = data.text
+const subst = `<:okthisisanemoji:$3>`;
+data.text = str.replace(regex, subst)
+         }
       if (data.text.includes("<:"))
       {
          if data.text.includes("<a"){
