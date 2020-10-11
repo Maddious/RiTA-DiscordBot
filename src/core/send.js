495c06a265d245be188972aaaa44a9a15cdab4db
Artanis_ <ledragio@gmail.com>
2020-10-11 19:23:07 -0400

    Update send.js

--- src/core/send.js
+++ src/core/send.js
@@ -61,36 +61,36 @@ module.exports = function(data)
          data.text = data.text.replace(/<А/gmi, "<a");
          if (data.text.includes("<А"))
          {
-            const regex1 = /<([:?\s:\s[a-z0-9ЁёА-я_A-Z\s\u00C0-\u017F]+\S*:\s*)([0-9\s]+)>/gmi;
+            const regex1 = /<(a)([:?\s:\s[a-z0-9ЁёА-я_A-Z\s\u00C0-\u017F]+\S*:\s*)([0-9\s]+)>/gmi;
             const str1 = data.text;
-            const subst1 = `<a:customemoji:$2>`;
+            const subst1 = `<a:customemoji:$3>`;
 
             data.text = str1.replace(regex1, subst1);
          }
          if (data.text.includes("<a"))
             {
-               const regex4 = /<([:?\s:\s[a-z0-9ЁёА-я_A-Z\s\u00C0-\u017F]+\S*:\s*)([0-9\s]+)>/gmi;
+               const regex4 = /<(a)([:?\s:\s[a-z0-9ЁёА-я_A-Z\s\u00C0-\u017F]+\S*:\s*)([0-9\s]+)>/gmi;
                const str4 = data.text;
-               const subst4 = `<a:customemoji:$2>`;
+               const subst4 = `<a:customemoji:$3>`;
 
                data.text = str4.replace(regex4, subst4);
          //   if a combination of animated emojis and normal custom emojis
          if (!data.text.includes("<a") && data.text.includes("<:"))
          {
-            /*if (data.text.includes("<a"))
-            {
-               const regex4 = /<([:?\s:\s[a-z0-9ЁёА-я_A-Z\s\u00C0-\u017F]+\S*:\s*)([0-9\s]+)>/gmi;
-               const str4 = data.text;
-               const subst4 = `<a:customemoji:$2>`;
-
-               data.text = str4.replace(regex4, subst4);*/
-            }
-            const subst5 = "<:customemoji:$2>";
+            
+            const subst5 = "<:customemoji:$3>";
             const str5 = data.text;
-            const regx5 = /<([:?\s:\s[a-z0-9ЁёА-я_A-Z\s\u00C0-\u017F]+\S*:\s*)([0-9\s]+)>/gmi;
+            const regx5 = /<:([:?\s:\s[a-z0-9ЁёА-я_A-Z\s\u00C0-\u017F]+\S*(:)\s*)([0-9\s]+)>/gmi;
 
             data.text = str5.replace(regx5, subst5);
          }
+          if (data.text.includes("<a") && data.text.includes("<:"))
+           {
+             const regex20 = /<(a)([:?\s:\s[a-z0-9ЁёА-я_A-Z\s\u00C0-\u017F]+\S*:\s*)([0-9\s]+)>/gmi;
+             const regex30 = /<:([:?\s:\s[a-z0-9ЁёА-я_A-Z\s\u00C0-\u017F]+\S*(:)\s*)([0-9\s]+)>/gmi;
+             data.text.replace(regex20, "<a:customemoji:$3>");
+             data.text.replace(regex30, "<:customemoji:$3>");
+                   }              
       }
    }
 
