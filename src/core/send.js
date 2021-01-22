fd47c2f7d6d73eee06cbb7a232001fef31d8b1ea
Artanis_ <ledragio@gmail.com>
2021-01-22 16:21:59 -0500

    Update send.js

--- src/core/send.js
+++ src/core/send.js
@@ -478,12 +478,7 @@ const embedOff = function(data)
             {
                // You can rename 'Webhook' to the name of your bot if you like, people will see if under the webhooks tab of the channel.
                existingWebhook = webhooks.find(x => x.name === webHookName);
-               webHookURL = "https://ritabot.org/index/images/favicon.png"
-               if (!data.bot.icon_url){
-               webHookURL = "https://ritabot.org/index/images/favicon.png"
-               } else {
-               webHookURL = data.bot.icon_url
-               };
+               const webHookURL = "https://ritabot.org/index/images/favicon.png"
 
                if (!existingWebhook)
                {
