cb410ed0b84f20a40e083a6bbda6059916b060be
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2021-04-10 22:24:19 +0100

    Update db.js

--- src/core/db.js
+++ src/core/db.js
@@ -60,6 +60,7 @@ const Servers = db.define("servers", {
       unique: true,
       allowNull: false
    },
+   prefix: Sequelize.STRING(8),
    lang: {
       type: Sequelize.STRING(8),
       defaultValue: "en"
@@ -326,6 +327,12 @@ exports.updateColumns = function(data)
    // Very sloppy code, neew to find a better fix.
    db.getQueryInterface().describeTable("servers").then(tableDefinition =>
    {
+      if (!tableDefinition.prefix)
+      {
+         console.log("-------------> Adding embedstyle column");
+         db.getQueryInterface().addColumn("servers", "prefix", {
+            type: Sequelize.STRING(8)});
+      }
       if (!tableDefinition.embedstyle)
       {
          console.log("-------------> Adding embedstyle column");
@@ -552,7 +559,8 @@ exports.getServerInfo = function(id, callback)
    `(select bot2botstyle as "bot2botstyle" from servers where id = ?) as table5, ` +
    `(select webhookactive as "webhookactive" from servers where id = ?) as table6,` +
    `(select webhookid as "webhookid" from servers where id = ?) as table7,` +
-   `(select webhooktoken as "webhooktoken" from servers where id = ?) as table8;`, { replacements: [ id, id, id, id, id, id, id, id],
+   `(select webhooktoken as "webhooktoken" from servers where id = ?) as table8,` +
+   `(select prefix as "prefix" from servers where id = ?) as table9;`, { replacements: [ id, id, id, id, id, id, id, id, id],
       type: db.QueryTypes.SELECT})
       .then(
          result => callback(result),
