1f1b46428b1f9831c3b5afb83ead924a9fd40c00
ArtanisTheOne <ledragio@gmail.com>
2021-04-11 17:41:52 -0400

    Set rotating status, every 20 seconds

--- src/core/status.js
+++ src/core/status.js
@@ -4,6 +4,8 @@
 
 module.exports = function(bot, status, config, writable = true)
 {
+   const activevar = [`ritabot.gg | ${config.translateCmdShort} help`, `for messages to translate | ritabot.gg`, "messages to translate | ritabot.gg", ` ${config.translateCmdShort} help commands | ritabot.gg`, "translations | ritabot.gg", `v.${config.version} | ritabot.gg`, `${config.translateCmdShort} help modules | ritabot.gg` ];
+   const statusvar = ["PLAYING", "WATCHING", "LISTENING", "WATCHING", "WATCHING", "PLAYING", "WATCHING"];
    const statusMap =
    {
       "online": function()
@@ -11,9 +13,28 @@ module.exports = function(bot, status, config, writable = true)
          bot.setPresence({
             status: "online",
             game: {
+               name: activevar[0]
+            } //run this on stratup
+
+         });
+         setInterval(function() //every 20 seconds generate a random number and update status to that
+         {
+            var actID = Math.floor(Math.random() * 6);
+            bot.setPresence({
+               status: "online",
+               game: {
+                  name: activevar[actID],
+                  type: statusvar[actID]
+               }
+            });
+         }, 20000);
+         /*const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
+         bot.setPresence({
+            status: "online",
+            game: {
                name: "ritabot.gg | " + config.translateCmdShort + " help" //V." + config.version
             }
-         });
+         });*/
       },
 
       "busy": function()
