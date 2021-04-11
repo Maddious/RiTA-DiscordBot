2b18597ab71db2630a4b9a49d40da47119f97f6b
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2021-04-11 21:07:17 +0100

    Update stats.js

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -72,9 +72,11 @@ module.exports = function(data)
                `:repeat:  Automatic translation:  ` +
                `**\`${activeServerTasks}\`**  channels and  ` +
                `**\`${data.cmd.server[0].activeUserTasks}\`**  users\n\n` +
+               `:robot: Users in Server: **\`${data.message.client.users.size}\`**\n\n` +
                `:inbox_tray: Embedded Message Status: **\`${embedVar}\`**\n\n` +
                `:robot: Bot to Bot Translation Status: **\`${bot2BotVar}\`**\n\n` +
                `:information_source: Webhook Debug Active State: **\`${webhookVar}\`**`;
+         
       }
 
       data.color = "info";
