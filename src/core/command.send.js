cae384d6743e25cfe3790e9d3b75a989e9a08c9f
ZyC0R3 <admin@zycore.co.uk>
2021-04-14 01:45:45 +0100

    Update command.send.js

--- src/core/command.send.js
+++ src/core/command.send.js
@@ -23,24 +23,9 @@ module.exports = async function(data)
    // Send Data to Channel
    // ---------------------
 
-   if (!process.env.DISCORD_BOT_OWNER_ID.includes(data.message.author.id))
+   if (process.env.DISCORD_BOT_OWNER_ID.includes(data.message.author.id))
    {
-      console.log("Insufficent Permission");
-      data.message.delete(5000).catch(err => console.log("Command Message Deleted Error, command.send.js = ", err));
-      richEmbedMessage
-         .setColor(colors.get(data.color))
-         .setAuthor(data.bot.username, data.bot.displayAvatarURL)
-         .setDescription(data.text)
-         .setTimestamp()
-         .setFooter("This message will self-destruct in one minute");
-      return data.message.channel.send(richEmbedMessage).then(msg =>
-      {
-         msg.delete(60000).catch(err => console.log("Bot Message Deleted Error, command.send.js = ", err));
-      });
-   }
-   else if (process.env.DISCORD_BOT_OWNER_ID.includes(data.message.author.id))
-   {
-      console.log("Developer Override ");
+      console.log("Developer Override");
       data.message.delete(5000).catch(err => console.log("Command Message Deleted Error, command.send.js = ", err));
       richEmbedMessage
          .setColor(colors.get(data.color))
