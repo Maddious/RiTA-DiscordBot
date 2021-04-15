464e1712aeacdb242cfcdde129419772f7db6e87
ZyC0R3 <admin@zycore.co.uk>
2021-04-15 04:07:51 +0100

    Update debug.js

--- src/commands/future_commands/debug.js
+++ src/commands/future_commands/debug.js
@@ -18,18 +18,21 @@ module.exports.run = function(data)
    // Command allowed by admins only
    // -------------------------------
 
-   if (data.message.isAdmin === false)
+   Override: if (!process.env.DISCORD_BOT_OWNER_ID.includes(data.message.author.id))
    {
-      data.color = "warn";
-      data.text = ":cop:  This command is reserved for server admins.";
+      if (data.message.isAdmin === false)
+      {
+         data.color = "warn";
+         data.text = ":cop:  This command is reserved for server admins.";
 
-      // -------------
-      // Send message
-      // -------------
+         // -------------
+         // Send message
+         // -------------
 
-      return sendMessage(data);
+         return sendMessage(data);
+      }
+      break Override;
    }
-
    // --------------------------------
    // Error if debug param is missing
    // --------------------------------
@@ -52,10 +55,7 @@ module.exports.run = function(data)
    // Execute setting
    // ----------------
 
-   if (data.message.isAdmin)
-   {
-      debug(data);
-   }
+   debug(data);
 };
 
 // -------------------------------
