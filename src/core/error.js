b6e20f553d8b16387cf93b6eae52f53495ebc149
JShep89 <46389911+JShep89@users.noreply.github.com>
2021-04-16 16:57:49 -0500

    Update error.js

--- src/core/error.js
+++ src/core/error.js
@@ -11,7 +11,19 @@
 
 module.exports = {
    "access": "50001",
+   "account": "10001",
    "content": "50035",
+   "general": "0",
    "perm": "50013",
-   "sendDm": "50007"
+   "sendDm": "50007",
+   "unknownApplication": "10002",
+   "unknownChannel": "10003",
+   "unknownGuild": "10004",
+   "unknownIntegration": "10005",
+   "unknownInvite": "10006",
+   "unknownMember": "10007",
+   "unknownMessage": "10008",
+   "unknownPermOverwrite": "10009",
+   "unknownProvider": "10010",
+   "unknownRole": "10011"
 };
