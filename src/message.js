5e58bc81c252d772695ed3617c0354193bed5656
ZyC0R3 <admin@zycore.co.uk>
2021-04-24 11:27:28 +0100

    Update message.js

--- src/message.js
+++ src/message.js
@@ -144,7 +144,7 @@ module.exports = function run (config, message, edited, deleted)
 
    {
 
-      if (message.content.startsWith(config.translateCmd) || message.content.startsWith(config.translateCmdShort) || message.isMentioned(bot))
+      if (message.content.startsWith(config.translateCmd) || message.content.startsWith(config.translateCmdShort) || message.mentions.has(bot))
       {
 
          // eslint-disable-next-line consistent-return
