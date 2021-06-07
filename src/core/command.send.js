d196da986cd904088e3be2de4336b67ce607a02f
Zycore <39091558+ZyC0R3@users.noreply.github.com>
2021-06-08 00:07:36 +0100

    Update command.send.js

--- src/core/command.send.js
+++ src/core/command.send.js
@@ -102,7 +102,7 @@ module.exports = function run (data)
       ));
       richEmbedMessage.
          setColor(colors.get(data.color)).
-         setDescription(`Developer Identity confirmed: \n\n${data.text}`).
+         setDescription(`Developer Identity confirmed:\n${data.text}`).
          setTimestamp().
          setFooter("This message will self-destruct in one minute");
       // -------------
