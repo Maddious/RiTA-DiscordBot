64b08ec155224ce98227f2c59005c3a1008aa6cd
JShep89 <46389911+JShep89@users.noreply.github.com>
2021-06-21 15:08:43 -0500

    Update help.js

--- src/commands/utility_commands/help.js
+++ src/commands/utility_commands/help.js
@@ -56,49 +56,49 @@ function helpMessage (config, param)
    helpSection({
       config,
       "icon": ":flag_white:",
-      "link": `<https://app.gitbook.com/@ritabot/s/rita-docs/usage-commands-and-settings/react>`,
+      "link": `<https://docs.ritabot.gg/rita-docs/usage-commands-and-settings/react>`,
       "title": "Translate by Reacting",
       "value": `${cmd} help react`
    }) +
    helpSection({
       config,
       "icon": ":abc:",
-      "link": `<https://app.gitbook.com/@ritabot/s/rita-docs/usage-commands-and-settings/custom>`,
+      "link": `<https://docs.ritabot.gg/rita-docs/usage-commands-and-settings/custom>`,
       "title": "Translate Custom Text",
       "value": `${cmd} help this`
    }) +
    helpSection({
       config,
       "icon": ":arrow_double_up:",
-      "link": `<https://app.gitbook.com/@ritabot/s/rita-docs/usage-commands-and-settings/last>`,
+      "link": `<https://docs.ritabot.gg/rita-docs/usage-commands-and-settings/last>`,
       "title": "Translate Last Message",
       "value": `${cmd} help last`
    }) +
    helpSection({
       config,
       "icon": ":hash:",
-      "link": `<https://app.gitbook.com/@ritabot/s/rita-docs/usage-commands-and-settings/chan>`,
+      "link": `<https://docs.ritabot.gg/rita-docs/usage-commands-and-settings/chan>`,
       "title": "Translate Channel (Automatic)",
       "value": `${cmd} help auto`
    }) +
    helpSection({
       config,
       "icon": ":bar_chart:",
-      "link": `<https://app.gitbook.com/@ritabot/s/rita-docs/usage-commands-and-settings/statistics>`,
+      "link": `<https://docs.ritabot.gg/rita-docs/usage-commands-and-settings/statistics>`,
       "title": "Stats",
       "value": `${cmd} help stats`
    }) +
    helpSection({
       config,
       "icon": ":gear:",
-      "link": `<https://app.gitbook.com/@ritabot/s/rita-docs/usage-commands-and-settings/settings>`,
+      "link": `<https://docs.ritabot.gg/rita-docs/usage-commands-and-settings/settings>`,
       "title": "Settings",
       "value": `${cmd} help settings`
    }) +
    helpSection({
       config,
       "icon": ":robot:",
-      "link": `<https://app.gitbook.com/@ritabot/s/rita-docs/usage-commands-and-settings/misc>`,
+      "link": `<https://docs.ritabot.gg/rita-docs/usage-commands-and-settings/misc>`,
       "title": "Misc. Settings",
       "value": `${cmd} help misc`
    });
