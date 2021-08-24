b2812f0e7d21c7948476f869dc5fce8a6fa5bc17
JShep89 <46389911+JShep89@users.noreply.github.com>
2021-08-24 01:55:38 -0500

    Update country.langs.js
    test

--- src/core/country.langs.js
+++ src/core/country.langs.js
@@ -1236,5 +1236,10 @@ module.exports = {
       "code": "zw",
       "name": "Zimbabwe",
       "langs": ["ny", "en", "xh", "st", "sn"]
+   },
+   "EA": {
+      "code": "ea",
+      "name": "Ceuta & Melilla",
+      "langs": ["es"]
    }
 };
