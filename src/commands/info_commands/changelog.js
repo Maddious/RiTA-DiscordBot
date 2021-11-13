e80c2d13dde5e76540f20b0f148c849cebaa7295
Zycore <admin@zycore.co.uk>
2021-11-13 04:21:40 +0000

    Update changelog.js

--- src/commands/info_commands/changelog.js
+++ src/commands/info_commands/changelog.js
@@ -10,11 +10,19 @@ module.exports = function run (data)
 
    data.color = "info";
    data.text = "```md\n" +
+      `# ---------------------- Stat  Changes ---------------------- \n` +
+      `* RITA has boomed so much since she was launched and now has \n\n` +
+      `  - Transalted over 26 Million messages\n` +
+      `     - Across 10k Servers \n` +
+      `         - For 1.1 Million Users \n\n` +
+      "```\n" +
+
+      "```md\n" +
       `# ---------------------- Major Changes ---------------------- \n` +
       `* CS same language now works \n` +
       `* Join/Leave logs can be seperated from error logs\n` +
-      `* Task ID added to tasks Printout \n\n` +
-      "```\n" +
+      `* Task ID added to tasks Printout \n` +
+      "```" +
 
       "```md\n" +
       `# ------------------------ Bug Fixes ------------------------ \n` +
@@ -23,21 +31,21 @@ module.exports = function run (data)
       `* Various Permission Errors \n` +
       `* Error messages show Task ID \n` +
       `* Ignore same chan translation of GIF and images. \n` +
-      `* Fixed translate for me command \n\n` +
-      "```\n" +
+      `* Fixed translate for me command \n` +
+      "```" +
 
       "```md\n" +
       `# --------------------- Command Changes --------------------- \n` +
-      `* !tr updatelink has been added.\n` +
-      `* !tr reactpersist has been added.\n` +
-      `* !tr flagpersist has been added. \n` +
-      `* !tr checkperms has ben replaced with !tr check \n` +
+      `* !tr updatelink has been added \n` +
+      `* !tr reactpersist has been added \n` +
+      `* !tr flagpersist has been added \n` +
+      `* !tr checkperms has been replaced with !tr check \n` +
       `* !tr stop task [id] - Removes a task by its given ID \n` +
       `* !tr settings tags [all/everyone/none] has been added \n` +
       `  - none - No tags are disabled \n` +
       `  - everyone - Attention tags are disabled \n` +
-      `  - all - All tags are disabled \n\n` +
-      "```\n" +
+      `  - all - All tags are disabled \n` +
+      "```" +
 
       "```md\n" +
       `# -------------------- Database  Changes -------------------- \n` +
@@ -48,16 +56,16 @@ module.exports = function run (data)
       `  - reactpersist - manages the react persist variable \n` +
       `  - flagpersist - manages the flag persist variable \n` +
       `  - menupersist - manages the menu persist variable \n` +
-      `  - whitelisted - NOT IN USE, Yet! \n\n` +
-      "```\n" +
+      `  - whitelisted - NOT IN USE, Yet! \n` +
+      "```" +
 
       "```md\n" +
       `# ---------------------- Misc. Changes ---------------------- \n` +
-      `* Help menu updated.\n` +
-      `* Owner variable is now more reliable on messages.\n` +
-      `* Source language added to embed on messages. \n` +
+      `* Help menu updated \n` +
+      `* Owner variable is now more reliable on messages \n` +
+      `* Source language added to embed on messages \n` +
       `* Owner, Admin & Channel Manager stored in message object \n` +
-      `* servertags status added to the settings and check commands \n\n` +
+      `* servertags status added to the settings and check commands \n` +
       "```\n";
 
    // -------------
